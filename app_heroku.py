e612b4944fa2263774656bc04f5cada0299e9fb2
Nelson <wx_nelson@yahoo.com>
2018-07-14 22:01:11 -0500

    retry

--- app_heroku.py
+++ app_heroku.py
@@ -1,4 +1,3 @@
-#import dependencies
 import os
 import io
 import numpy as np
@@ -11,23 +10,20 @@ from keras import backend as K
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
-#set up Flask
+
 app = Flask(__name__)
-#app.config['UPLOAD_FOLDER'] = 'tmp'
 app.config['UPLOAD_FOLDER'] = 'static'
-#get the current dictory name
-MYDIR = os.path.dirname(__file__)
+app.config['UPLOAD_FOLDER_MODEL'] = 'upload_models'
+
+WORKING_FOLDER = os.path.dirname(__file__)
+
 #set age to 0 to refresh image saved in the tmp folder
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-#load fine tuned model
-mymodel = load_model('my_model_final.h5')
-
-#initiate tensorflow graph and session
+mymodel = None
 graph = K.get_session().graph
 
 
-#define a function to pre-process image
 def prepare_image(img):
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
@@ -36,53 +32,69 @@ def prepare_image(img):
     # return the processed image
     return img
 
-#define index route
+
+def save_file(request, filename, config_folder):
+    if request.files.get('file'):
+        filepath = os.path.join(app.config[config_folder], filename)
+        file = request.files['file']
+        file.save(filepath)
+        return filepath
+    else:
+        return None
+
+
+def reload_model(name):
+    global mymodel
+    global graph
+    with graph.as_default():
+        mymodel = load_model('upload_models/{}'.format(name))
+
+
+@app.route('/model_upload', methods=['GET', 'POST'])
+def model_upload():
+    if request.method == 'GET':
+        return render_template("model_upload.html", result='')
+    else:
+        if request.form.get('secret', "") == 'mySecretCode984736':
+            save_file(request, 'my_model_final.h5', 'UPLOAD_FOLDER_MODEL')
+            reload_model('my_model_final.h5')
+            return render_template("model_upload.html",
+                    result='Model reloaded.', msg='Ready to predict!')
+        else:
+            return render_template("model_upload.html", result='Bad secret code.')
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     output = {"success": False}
     if request.method == 'POST':
-        #if a file is selected and uploaded
         if request.files.get('file'):
-            #read the file
-            file = request.files['file']
 
-            #specify the file name
-            filename = 'upload.jpg'
+            filepath = save_file(request, 'upload.jpg', 'UPLOAD_FOLDER')
 
-            #create a path to the uploads folder
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            #filepath = os.path.join(MYDIR + "/" + app.config['UPLOAD_FOLDER'], filename)
-            #save file
-            file.save(filepath)
-
-            #load the saved image using Keras and resize it to the format of 224x224 pixels
+            # Load the saved image using Keras and resize it to the Xception
+            # format of 299x299 pixels
             image_size = (224, 224)
             im = image.load_img(filepath, target_size=image_size, grayscale=False)
 
-            #preprocess the image and prepare it for classification
+            # preprocess the image and prepare it for classification
             processed_image = prepare_image(im)
 
-            #define the list of flower names
             flower_list = ['daisy','dandelion','rose','sunflower','tulip']
-            
+
             global graph
             with graph.as_default():
-                #use the model to predict the preprocessed image
                 predictions = mymodel.predict(processed_image)
-                #get the index number of predictions
                 predict_index = np.argmax(predictions, axis=1)[0]
-                #use the index to look up the flower name from the list of flowers
+
                 result = flower_list[predict_index]
-                #extract the predict probabability/confidence number
                 probability = float(predictions[0][predict_index])
-                #save prediction results and probability to output dictionary
+
                 output["flower"] = result
                 output["probability"] = probability
 
-                #indicate that the request was a success
+                # indicate that the request was a success
                 output["success"] = True
 
-            #pass output to index html and render the page
             return render_template("index.html",result=output)
 
 
@@ -90,4 +102,6 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # get port number of Heroku.  If not specified, default to 5000
+    env_port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=env_port, debug=True)
