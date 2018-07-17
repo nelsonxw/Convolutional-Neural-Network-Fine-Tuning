fd485ebe7c800833d197467f0c6e6a267ae652f4
Nelson <wx_nelson@yahoo.com>
2018-07-17 14:55:55 -0500

    update index.js

--- static/index.js
+++ static/index.js
@@ -1,11 +1,16 @@
+/*if there is a prediction result, show picture and wiki page*/
 if (input != 'none') {
 	var picture = document.getElementById("picture");
+	/*if previous picture exists, delete it first*/
 	picture.innerHTML = '';
 	var img = document.createElement("img");
+	/*upload image*/
 	img.setAttribute("src","static/upload.jpg");
+	img.style.width = '200px';
 	/*img.setAttribute("src","https://test-machine-learning.herokuapp.com/images");*/
 	picture.appendChild(img);
 	var finding = document.getElementById("finding");
+	/*if prediction probability or confidence is low, less than 50%, show message instead of wiki page*/
 	if (probability <= 0.5) {
 		var node = document.createElement("p");
 		var message1 = document.createElement('span')
@@ -19,6 +24,7 @@ if (input != 'none') {
 		node.appendChild(message2);
 		finding.appendChild(node);
 	} else {
+		/*embed an iframe to the site, and direct to wiki page of the predicted flower*/
 		var node = document.createElement("iframe");
 		node.setAttribute("src","https://en.wikipedia.org/wiki/" + flower)
 		node.setAttribute("style","height: 400px; width: 100%")
